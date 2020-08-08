35a34b5265abd4fa8cf638f4192a3c51b58d6dd9
Roshan Elvitigala <roshanrox911@gmail.com>
2020-08-08 20:21:53 +1000

    slot machie another solu

--- Machine.js
+++ Machine.js
@@ -1,9 +1,12 @@
 class Machine extends React.Component{
     render(){
+        const {s1, s2, s3 } = this.props;
+        const winner = (s1 === s2) && (s2 === s3);
         return(
         <div>
             <div>{this.props.s1} {this.props.s2} {this.props.s3}</div>
-        <p>{this.props.s1 === this.props.s2? (this.props.s2 === this.props.s3? "You win!" : "You lose!") : "You lose!"}</p>
+        <p>{winner? 'Winner!' : 'Loser!'}</p>
+        {/* <p>{this.props.s1 === this.props.s2? (this.props.s2 === this.props.s3? "You win!" : "You lose!") : "You lose!"}</p> */}
         </div>
         )
     }
