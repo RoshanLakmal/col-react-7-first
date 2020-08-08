8f382384f1a8138e28005c32e137bebe480537ce
Roshan Elvitigala <roshanrox911@gmail.com>
2020-08-08 18:33:41 +1000

    random num

--- index.js
+++ index.js
@@ -10,13 +10,20 @@
 // 	}
 // }
 
-function Hello() {
-	return(
-		<div>
-			<h1>Hello there!</h1>
-			<h1>Hello there!</h1>
-			<h1>Hello there!</h1>
-		</div>
-	);
+function getNum(){
+	return Math.floor(Math.random() *10)+1;
 }
-ReactDOM.render(<Hello />, document.getElementById('root'));
+
+class NumPicker extends React.Component{
+	
+	render(){
+		const num = getNum();
+		return(
+			<div>
+				<h1>Your number is {num}</h1>
+				<p>{num===7? "Congratulations" : "Try again"}</p>
+			</div>
+		);
+	}
+}
+ReactDOM.render(<NumPicker />, document.getElementById('root'));
