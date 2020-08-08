de7d30769392321c1c830bddc9a738c36b87cabd
Roshan Elvitigala <roshanrox911@gmail.com>
2020-08-08 20:33:16 +1000

    added loop list with map

--- index.js
+++ index.js
@@ -55,26 +55,40 @@
 // 	}
 // }
 
+// class App extends React.Component{
+	
+// 	render(){
+// 		return(
+// 			<div>
+// 				<h1>Slot Machine!</h1>
+// 				<Machine 
+// 					s1="x"
+// 					s2="y"
+// 					s3="z"
+// 				/>
+// 				<Machine 
+// 					s1="x"
+// 					s2="x"
+// 					s3="x"
+// 				/>
+// 				<Machine 
+// 					s1="x"
+// 					s2="y"
+// 					s3="x"
+// 				/>
+// 			</div>
+// 		);
+// 	}
+// }
+
 class App extends React.Component{
 	
 	render(){
 		return(
 			<div>
-				<h1>Slot Machine!</h1>
-				<Machine 
-					s1="x"
-					s2="y"
-					s3="z"
-				/>
-				<Machine 
-					s1="x"
-					s2="x"
-					s3="x"
-				/>
-				<Machine 
-					s1="x"
-					s2="y"
-					s3="x"
+				<Friend 
+					name="Roshan"
+					hobbies= {["Anime","Cricket","Movie"]}
 				/>
 			</div>
 		);
